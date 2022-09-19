456ec451338e8ad58a58a116c9c0ef3c49d7febb
benorium <benorium@gmail.com>
2022-09-19 16:09:04 +0300

    aded commits for code

--- test00/Program.cs
+++ test00/Program.cs
@@ -1,25 +1,24 @@
-﻿string[] array1 = new string[5] {"Hello", "2", "world", ":-)", };
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+
+
+
+string[] array1 = new string[4] {"Hello", "2", "world", ":-)", }; //Для изменения первоначального массива можно доваить или изменить данные в поле {...} и изменить размер в поле []
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
-        {
-        array2[count] = array1[i];
-        count++;
-        }
-    }
-}
-string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
-string[] array2 = new string[array1.Length];
-void SecondArrayWithIF(string[] array1, string[] array2)
-{
-    int count = 0;
-    for (int i = 0; i < array1.Length; i++)
-    {
-    if(array1[i].Length <= 3)
+    if(array1[i].Length <= 3)  //Для изменения размера нового массива следует изменить параметр 3 на необходимый
         {
         array2[count] = array1[i];
         count++;
